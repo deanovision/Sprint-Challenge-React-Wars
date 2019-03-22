e7e17eb760cf645fd9490c8951172fb4e99e131b
deanovision <rodeanfraser@gmail.com>
2019-03-22 10:50:20 -0400

    added more info to Character component

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,7 +3,11 @@ import "./StarWars.css";
 const Character = props => {
   return (
     <div>
-      <h2>Name: {props.characterProps.name}</h2>
+      <h3>Name: {props.characterProps.name}</h3>
+      <p>Born in Year: {props.characterProps.birth_year}</p>
+      <p> Height: {props.characterProps.height}</p>
+      <p> Mass: {props.characterProps.mass}</p>
+      <p> Gender: {props.characterProps.gender}</p>
     </div>
   );
 };
